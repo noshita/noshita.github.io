9d3c8ead1036f162760fffcbaf5fab79c1aad147
Koji Noshita <noshita@ut-biomet.org>
2015-12-01 23:46:36 +0900

    Delete config

---
+++ publishconf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
+
+import os
+import sys
+sys.path.append(os.curdir)
+from pelicanconf import *
+
+SITEURL = 'http://noshita.github.io'
+RELATIVE_URLS = False
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+
+DELETE_OUTPUT_DIRECTORY = True
+
+
+# Following items are often useful when publishing
+
+import json
+with open(os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.json")), "r") as f:
+	config = json.load(f) 
+DISQUS_SITENAME = config["DISQUS_SITENAME"]
+GOOGLE_ANALYTICS = config["GOOGLE_ANALYTICS"]
